8f19f8e0894058553d53d497836b1d06641e3209
juliaklzs <juliakalazanslm@gmail.com>
2023-03-25 04:46:22 -0300

    implementação toString

--- java-basico/data-structure/src/com/project/doublylinkedlist/DoublyLinkedList.java
+++ java-basico/data-structure/src/com/project/doublylinkedlist/DoublyLinkedList.java
@@ -30,6 +30,45 @@ public class DoublyLinkedList<T> {
         lastKnot = newKnot;
         listSize++;
     }
+
+    public void add(int index, T element) {
+        DoubleKnot<T> auxKnot = getKnot(index);
+        DoubleKnot<T> newKnot = new DoubleKnot<>(element);
+        newKnot.setNextKnot(auxKnot);
+
+        if(newKnot.getNextKnot() != null) {
+            newKnot.setPreviousKnot(auxKnot.getPreviousKnot());
+            newKnot.getNextKnot().setPreviousKnot(newKnot);
+        } else {
+            newKnot.setPreviousKnot(lastKnot);
+            lastKnot = newKnot;
+        }
+
+        if(index == 0) {
+            firstKnot = newKnot;
+        } else {
+            newKnot.getPreviousKnot().setPreviousKnot(newKnot);
+        }
+        listSize++;
+    }
+
+    public void remove(int index) {
+        if(index == 0) {
+            firstKnot = firstKnot.getNextKnot();
+            if (firstKnot != null) {
+                firstKnot.setPreviousKnot(null);
+            }
+        } else {
+            DoubleKnot<T> auxKnot = getKnot(index);
+            auxKnot.getPreviousKnot().setNextKnot(auxKnot.getNextKnot());
+            if (auxKnot != lastKnot) {
+                auxKnot.getNextKnot().setPreviousKnot(auxKnot.getPreviousKnot());
+            } else {
+                lastKnot = auxKnot;
+            }
+        }
+        this.listSize--;
+    }
     
     private DoubleKnot<T> getKnot(int index) {
         DoubleKnot<T> auxKnot = firstKnot;
@@ -44,4 +83,16 @@ public class DoublyLinkedList<T> {
         return this.listSize;
     }
 
+    @Override
+    public String toString() {
+        String strReturn = "";
+
+        DoubleKnot<T> auxKnot = firstKnot;
+        for (int i = 0; i < size(); i++) {
+            strReturn += "[Knot{content=" + auxKnot.getContent() + "}]--->";
+            auxKnot = auxKnot.getNextKnot();
+        }
+        strReturn += "null";
+        return strReturn;
+    }
 }
