d9ca72b4435f67493f84d64eedb7de98df8312dc
juliaklzs <juliakalazanslm@gmail.com>
2023-03-29 05:00:56 -0300

    nó circular buffer

---
+++ java-basico/data-structure/src/com/project/circularbuffer/Knot.java
@@ -0,0 +1,35 @@
+package com.project.circularbuffer;
+
+public class Knot<T> {
+
+    private T content;
+    private Knot<T> nextKnot;
+
+    public Knot(T content) {
+        this.nextKnot = null;
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public Knot<T> getNextKnot() {
+        return nextKnot;
+    }
+
+    public void setNextKnot(Knot<T> nextKnot) {
+        this.nextKnot = nextKnot;
+    }
+
+    @Override
+    public String toString() {
+        return "Knot{" +
+                "content=" + content +
+                '}';
+    }
+}
