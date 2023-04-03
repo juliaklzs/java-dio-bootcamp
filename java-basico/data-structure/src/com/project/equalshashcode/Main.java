c0ca33b5f254d6e825d2569ad25b52359fdece81
juliaklzs <juliakalazanslm@gmail.com>
2023-04-03 16:59:38 -0300

    equals e hash code

---
+++ java-basico/data-structure/src/com/project/equalshashcode/Main.java
@@ -0,0 +1,22 @@
+package com.project.equalshashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Car> carList = new ArrayList<>();
+
+        carList.add(new Car("Ford"));
+        carList.add(new Car("Chevrolet"));
+        carList.add(new Car("Fiat"));
+
+        System.out.println(carList.contains(new Car("Ford")));
+        System.out.println(new Car("Ford").hashCode());
+
+        Car car1 = new Car("Ford");
+        Car car2 = new Car("Chevrolet");
+
+        System.out.println(car1.equals(car2));
+    }
+}
